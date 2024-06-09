ce51d8a99b02d2a177e2d9201a862aed85b48380
Ismayil Ceferli <ismayilej@code.edu.az>
2024-06-09 23:50:37 +0400

    Added Login & Register pages

---
+++ Project/Project/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+    }
+}
--- Project/Project/obj/Debug/net6.0/Project.AssemblyInfo.cs
+++ Project/Project/obj/Debug/net6.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a97cb00f2ce9bfaaae308d1358a048f0dea0604")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e866fe3f28f2931dc92b7d89f6132a4b2304bba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
